0fc6a17fe93f65a963c2b9a1d577a41c7f707563
Grzegorz Kaczor <grzesiek125@op.pl>
2021-10-03 19:29:40 +0200

    form validation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,40 @@
 $(function () {
 
+    $('.form-button').click(function(){
+        validForm();
+    });
+
+    function validForm() {
+        const firstname = $("#firstname").val();
+        const lastname = $("#lastname").val();
+        const email = $("#email").val();
+        const phoneNumber = $("#phone-number").val();
+        const postCode = $("#post-code").val();
+        const houseNumber = $("#house-number").val();
+
+        const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        const postCodeFormat = /^\d\d-\d\d\d$/;
+
+        $('.error').hide();
+
+        if (firstname == ""){
+            $("#firstname").after('<span class="error">Proszę wpisać imię!</span>')
+        }
+        if (lastname == ""){
+            $("#lastname").after('<span class="error">Proszę wpisać nazwisko!</span>')
+        }
+        if ((email == "") || (!email.match(mailFormat))){
+            $("#email").after('<span class="error">Proszę wpisać poprawny adres email!</span>')
+        }
+        if ((phoneNumber == "") || (phoneNumber.length > 9) || (phoneNumber.length < 9)){
+            $("#phone-number").after('<span class="error">Proszę wpisać numer telefonu!</span>')
+        }
+        if ((postCode == "") || (!postCode.match(postCodeFormat))){
+            $("#post-code").after('<span class="error">Proszę wpisać kod pocztowy!</span>')
+        }
+        if (houseNumber == "") {
+            $("#house-number").after('<span class="error">Proszę wpisać numer domu!</span>')
+        }
+    }
 
  });
